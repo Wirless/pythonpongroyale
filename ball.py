6e5510efb6f3528e824e70e26a5f47df666593d1
Henrik Petersson <henrik@tutamail.com>
2019-10-18 13:44:42 +0200

    Reformat code with black and isort
    Sorted imports and fixed mixed spaces and tabs.

--- ball.py
+++ ball.py
@@ -1,5 +1,7 @@
-import pygame
 from random import randint
+
+import pygame
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (78, 255, 87)
@@ -8,26 +10,26 @@ BLUE = (80, 255, 239)
 PURPLE = (203, 0, 255)
 RED = (237, 28, 36)
 
-class Ball(pygame.sprite.Sprite):
-	def __init__(self, color, width, height):
-		super().__init__()
-		
-		self.image = pygame.Surface([width, height])
-		self.image.fill(BLACK)
-		self.image.set_colorkey(BLACK)
-		
-		pygame.draw.rect(self.image, color, [0, 0, width, height])
-		self.velocity = [randint(2,4),randint(-2,6)]
-		self.rect = self.image.get_rect()
-		self.rotation = 0
-		
-	def update(self):
-		self.rect.x += self.velocity[0]
-		self.rect.y += self.velocity[1]
-		
-	def bounce(self):
-		#delay to let the ball move before it rebounces itself.
-		#depracated pygame.time.delay(100)
-		self.velocity[0] = -self.velocity[0]
-		self.velocity[1] = randint(-8,8)
 
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.velocity = [randint(2, 4), randint(-2, 6)]
+        self.rect = self.image.get_rect()
+        self.rotation = 0
+
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def bounce(self):
+        # delay to let the ball move before it rebounces itself.
+        # depracated pygame.time.delay(100)
+        self.velocity[0] = -self.velocity[0]
+        self.velocity[1] = randint(-8, 8)
