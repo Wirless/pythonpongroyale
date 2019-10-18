6d54316a6bb0ae5dc0309b9cea8012e8ceae6e52
Wirless <selppkez@gmail.com>
2019-10-18 11:01:45 +0100

    Update ball.py

--- ball.py
+++ ball.py
@@ -19,13 +19,15 @@ class Ball(pygame.sprite.Sprite):
 		pygame.draw.rect(self.image, color, [0, 0, width, height])
 		self.velocity = [randint(2,4),randint(-2,6)]
 		self.rect = self.image.get_rect()
+		self.rotation = 0
 		
 	def update(self):
 		self.rect.x += self.velocity[0]
 		self.rect.y += self.velocity[1]
 		
 	def bounce(self):
+		#delay to let the ball move before it rebounces itself.
+		pygame.time.delay(100)
 		self.velocity[0] = -self.velocity[0]
 		self.velocity[1] = randint(-8,8)
-
 
