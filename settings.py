8d20d0f7b7ecb31946a7fd87a4c1653cf5459d88
Wirless <selppkez@gmail.com>
2019-10-19 20:26:08 +0100

    settings.py
    more variables and options

--- settings.py
+++ settings.py
@@ -6,9 +6,15 @@ YELLOW = (241, 255, 0)
 BLUE = (80, 255, 239)
 PURPLE = (203, 0, 255)
 RED = (237, 28, 36)
-
+COLOR = RED
+#self.velocity[1] min,max
+BALLSPEED1 = -14.00
+BALLSPEED2 = 14.00
 # Wall borders where the ball will bounce.
 TOP_WALL = 0
 BOTTOM_WALL = 690
 LEFT_WALL = 0
 RIGHT_WALL = 690
+#DEBUG AND BOTS!
+DEBUG = False
+BOTS = False
