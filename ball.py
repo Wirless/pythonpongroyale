9290b33d5f82ef804c9e0eec512c8730f92a1d5d
Wirless <selppkez@gmail.com>
2019-10-19 20:25:01 +0100

    ball.py
    SETTINGS!

--- ball.py
+++ ball.py
@@ -14,18 +14,21 @@ class Ball(pygame.sprite.Sprite):
         self.image.set_colorkey(settings.BLACK)
 
         pygame.draw.rect(self.image, color, [0, 0, size[0], size[1]])
-        self.velocity = [uniform(2.3, 6.3), uniform(-2.3, 6.3)]
+        self.velocity = [uniform(2.0, 6.0), uniform(-2.0, 6.0)]
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
         self.rotation = 0
+        self.bouncecount = 0
 
     def update(self):
         self.rect.x += self.velocity[0]
         self.rect.y += self.velocity[1]
 
     def bounce(self):
-        
-        # delay to let the ball move before it rebounces itself.
-        # depracated pygame.time.delay(100)
         self.velocity[0] = -self.velocity[0]
-        self.velocity[1] = uniform(-8.0, 8.0)
+        self.velocity[1] = uniform(settings.BALLSPEED1, settings.BALLSPEED2)
+        self.bouncecount+=1
+    def bouncevertical(self):
+        self.velocity[0] = uniform(settings.BALLSPEED1, settings.BALLSPEED2)
+        self.velocity[1] = -self.velocity[1]
+        self.bouncecount+=1
